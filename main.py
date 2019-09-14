4ecff569fb5754b5d58eaa60636c4ce8a09f760b
kyohei <h27.kubb@gmail.com>
2019-09-14 14:00:08 +0900

    change the message type to list

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -31,26 +32,15 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#      line_bot_api.reply_message(
-#      	event.reply_token,
-#         TextSendMessage(text="お疲れさま"))
-
-
 def handle_message(event):
     line_bot_api.reply_message(
-    	event.reply_token,
-	ImageSendMessage(original_content_url="https://symfo.web.fc2.com/sample_src/lena.jpg", 
-			 preview_image_url="https://symfo.web.fc2.com/sample_src/lena_preview.jpg")
-    )
-
-    line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text="いつもお疲れさま")
+        event.reply_token,
+        [ImageSendMessage(original_content_url="https://symfo.web.fc2.com/sample_src/lena.jpg",
+                          preview_image_url="https://symfo.web.fc2.com/sample_src/lena_preview.jpg"),
+         TextSendMessage(text="いつもお疲れさま")]  # messageは最大５件 配列にして渡す
     )
 
 
 if __name__ == "__main__":
-#    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
